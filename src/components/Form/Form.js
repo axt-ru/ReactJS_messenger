2e0e4091be54c219b5f8fde15fefb092a7ed18e7
axt-ru <axtrr@mail.ru>
2021-08-02 18:07:50 +0300

    lesson3 - 5 task items

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,21 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { AUTHORS } from '../../constants';
 import './Form.css';
+import TextField from '@material-ui/core/TextField';
 
 export const Form = ({ onSendMessage }) => {
   const [value, setValue] = useState('');
-  const inputRef = useRef();
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [])
 
   const handleChange = (e) => {
     setValue(e.target.value);
+  }
+
+  const focusInput = () => {
+    inputRef.current.focus();
   }
 
   const handleSubmit = (e) => {
@@ -19,12 +27,14 @@ export const Form = ({ onSendMessage }) => {
       text: value,
     });
     setValue('');
-  }
+    inputRef.current?.focus();
+}
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input className="textField" ref={inputRef} type="text" value={value} onChange={handleChange} />
-      <input className="buttonChat" type="submit" />
+return (
+    <form onSubmit={handleSubmit} ref={inputRef}> 
+      <TextField id="standard-basic" ref={inputRef} label="Ваш текст" variant="filled" className="textField" type="text" value={value} onChange={handleChange} onLoad={focusInput}></TextField>
+      {/* <input className="textField" ref={inputRef} type="text" value={value} onChange={handleChange} /> */}
+      <input className="buttonChat" type="submit" onClick={handleSubmit}  />
     </form>
   )
 }
